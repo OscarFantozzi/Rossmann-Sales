2364945cf8dd8cdcc2aeb8af6a8ede231b4d5ee8
Oscar Fantozzi <110399283+OscarFantozzi@users.noreply.github.com>
2025-02-02 22:14:02 +0000

    Update handler.py

--- api/handler.py
+++ api/handler.py
@@ -1,13 +1,14 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'modules')))
 import pickle
 import pandas as pd
 from flask             import Flask, request
 from Rossmann import Rossmann
 
+model_path = os.path.join(os.path.dirname(__file__), '..', 'models', 'trained_model.pkl')
+
 # loading model
-model = pickle.load( open( r'models\trained_model.pkl' , 'rb') )
+model = pickle.load( open( model_path , 'rb') )
 
 # initialize api
 app = Flask( __name__ )
