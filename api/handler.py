5be3bdca80b9bdd6c3aa55a03bf827ef8ec26cf6
Oscar Fantozzi <110399283+OscarFantozzi@users.noreply.github.com>
2025-02-02 22:11:22 +0000

    Update handler.py

--- api/handler.py
+++ api/handler.py
@@ -7,7 +7,7 @@ from flask             import Flask, request
 from Rossmann import Rossmann
 
 # loading model
-model = pickle.load( open( r'..\models\trained_model.pkl' , 'rb') )
+model = pickle.load( open( r'models\trained_model.pkl' , 'rb') )
 
 # initialize api
 app = Flask( __name__ )
