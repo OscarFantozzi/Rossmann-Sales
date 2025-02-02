20099a180a36322861e4b96fbe50b3360e33ad22
Oscar Fantozzi <110399283+OscarFantozzi@users.noreply.github.com>
2025-02-02 22:46:30 +0000

    Update Rossmann.py

--- modules/Rossmann.py
+++ modules/Rossmann.py
@@ -9,7 +9,7 @@ class Rossmann( object ):
     def __init__( self ):
         parameter_path = os.path.join(os.path.dirname(__file__), '..', 'parameter')
         self.competition_distance_scaler   = pickle.load( open(os.path.join(parameter_path, 'competition_distance_scaler.pkl') , 'rb' ) )
-        self.competition_time_month_scaler = pickle.load( open(os.path.join(parameter_path,'competition_time_month_scaler.pkl' , 'rb' ) )
+        self.competition_time_month_scaler = pickle.load( open(os.path.join(parameter_path,'competition_time_month_scaler.pkl') , 'rb' ) )
         self.promo_time_week_scaler        = pickle.load( open(os.path.join(parameter_path, 'promo_time_week_scaler.pkl', 'rb' ) )
         self.year_scaler                   = pickle.load( open(os.path.join(parameter_path, 'year_scaler.pkl' , 'rb' ) )
         self.store_type_scaler             = pickle.load( open(os.path.join(parameter_path, 'store_type_scaler.pkl' , 'rb' ) )
